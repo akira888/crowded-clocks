e2eb0440deab9d29c039768c25b8f5f9f2950e8f
akira888 <a.cup.of.happiness@gmail.com>
2024-10-09 10:19:48 +0900

    make 3:00

---
+++ src/components/BasicClock.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="clocks">
+    <div class="rim">
+      <span class="big-hand"></span>
+      <span class="small-hand"></span>
+    </div>
+  </div>
+</template>
---
+++ src/views/CrowdedClocks.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+//import { ref } from 'vue'
+import BasicClock from '../components/BasicClock.vue'
+</script>
+
+<template>
+  <div id="grid">
+    <BasicClock />
+  </div>
+</template>

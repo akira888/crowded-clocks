b56c2b56f538b560b2dee559beb84c118541c8f5
akira888 <a.cup.of.happiness@gmail.com>
2024-10-10 03:21:42 +0900

    角度のrefを調整しやすいように変更、動作確認ツール設置

--- src/components/BasicClock.vue
+++ src/components/BasicClock.vue
@@ -1,10 +1,15 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-const smallHandDegree = ref('1deg')
-const bigHandDegree = ref('20deg')
+const smallHandDegree = ref<number>(90)
+const bigHandDegree = ref<number>(180)
 </script>
 
 <template>
+  <div class="controll-panel">
+    <input type="range" min="0" max="360" v-model="smallHandDegree" /><b>minutes</b>
+    <br />
+    <input type="range" min="0" max="360" v-model="bigHandDegree" /><b>hour</b>
+  </div>
   <div class="clocks">
     <div class="rim">
       <span class="big-hand"></span>
@@ -15,9 +20,12 @@ const bigHandDegree = ref('20deg')
 
 <style scoped>
 .big-hand {
-  transform: rotate(v-bind(bigHandDegree));
+  transform: rotate(v-bind(bigHandDegree + 'deg'));
 }
 .small-hand {
-  transform: rotate(v-bind(smallHandDegree));
+  transform: rotate(v-bind(smallHandDegree + 'deg'));
+}
+b {
+  color: blueviolet;
 }
 </style>

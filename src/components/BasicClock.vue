3f1df61d9d2e9c978f80ef13a56999aa9d370c8a
akira888 <a.cup.of.happiness@gmail.com>
2024-10-13 01:14:10 +0900

    時計として動く状態

--- src/components/BasicClock.vue
+++ src/components/BasicClock.vue
@@ -1,7 +1,20 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-const smallHandDegree = ref<number>(90)
-const bigHandDegree = ref<number>(180)
+import { ref, computed } from 'vue'
+const props = defineProps(['times'])
+const defaultDegree: number = 90.0
+const smallHandDegree = computed((): number => {
+  const minutes = props.times.minutes
+  const seconds = props.times.seconds
+  let degree = 6 * minutes + 0.1 * seconds
+  return degree + defaultDegree
+})
+const bigHandDegree = computed((): number => {
+  const hours = props.times.hours % 12
+  const minutes = props.times.minutes
+  let degree = 30 * hours
+  degree += minutes > 0 ? 0.5 * minutes : 0
+  return degree + defaultDegree
+})
 </script>
 
 <template>
--- src/views/CrowdedClocks.vue
+++ src/views/CrowdedClocks.vue
@@ -1,10 +1,43 @@
 <script setup lang="ts">
-//import { ref } from 'vue'
+import { reactive, ref } from 'vue'
 import BasicClock from '../components/BasicClock.vue'
+const date = new Date()
+const reset = ref<number>(0)
+const times: any = reactive({
+  hours: date.getHours(),
+  minutes: date.getMinutes(),
+  seconds: date.getSeconds(),
+  msec: date.getMilliseconds()
+})
+setTimeout(startClock, 999 - times.msec)
+
+function getTime() {
+  const date = new Date()
+  times.hours = date.getHours()
+  times.minutes = date.getMinutes()
+  times.seconds = date.getSeconds()
+  times.msec = date.getMilliseconds()
+
+  // リセット機構
+  if (times.msec > 20 && times.msec < 980) {
+    clearInterval(reset.value)
+    setTimeout(startClock, 999 - times.msec)
+  }
+}
+
+function startClock() {
+  getTime()
+  reset.value = setInterval(getTime, 1000)
+}
 </script>
 
 <template>
+  <h3>digital clock</h3>
+  <div>
+    <time>{{ times.hours + ':' + times.minutes + ':' + times.seconds }}</time>
+    msec: {{ times.msec }}
+  </div>
   <div id="grid">
-    <BasicClock />
+    <BasicClock :times="times" />
   </div>
 </template>

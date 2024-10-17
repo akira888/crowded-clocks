afcab519eb3ff970d53501f2ed905f93c404cfc6
akira888 <a.cup.of.happiness@gmail.com>
2024-10-17 21:10:57 +0900

    ポジション指定子を再利用可能に

--- src/services/DigitDeclare.ts
+++ src/services/DigitDeclare.ts
@@ -1,18 +1,16 @@
-const u: number = 0
-const r: number = 90
-const d: number = 180
-const l: number = 270
-const tlCorner: number[] = [r, d]
-const trCorner: number[] = [l, d]
-const blCorner: number[] = [r, u]
-const brCorner: number[] = [l, u]
-const wu: number[] = [u, u]
-const wd: number[] = [d, d]
-const wl: number[] = [l, l]
-const wr: number[] = [r, r]
-const hLine: number[] = [r, l]
-const vLine: number[] = [u, d]
-const empty: null[] = [null, null]
+import {
+  tlCorner,
+  trCorner,
+  blCorner,
+  brCorner,
+  wu,
+  wd,
+  wl,
+  wr,
+  hLine,
+  vLine,
+  empty
+} from './PositionSymbols'
 
 interface StringKeyObject {
   [key: string]: any[]
